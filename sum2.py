c0dc761715f8d53860177048c4cbc0ed9456269a
Jonathan Yergin <jonathanyergin@Jonathans-MacBook-Pro.local>
2019-09-04 22:54:43 -0400

    create new examples

---
+++ sum2.py
@@ -0,0 +1,10 @@
+# Python3 program to add two numbers from user input
+
+number1 = input("First number: ")
+number2 = input("\nSecond Number: ")
+
+# adding two numbers
+sum = float(number1) + float(number2)
+
+# Display sum, printing the values in float format
+print("The sum of {0} and {1} is {2}".format(number1, number2, sum))
